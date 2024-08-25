b69aa1d76e4ea7fbd8527430cffab615b8377fd6
emperorzzr <firm48576@gmail.com>
2024-08-25 16:08:59 +0700

    Add files via upload

---
+++ precache-manifest.feb9f0e74fd247ce469b4a80cbcda4bf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593f250cba623f257239cdf56d6ceab9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b06cafa38bbd6d2c2e07",
+    "url": "./static/css/main.c93c0307.chunk.css"
+  },
+  {
+    "revision": "135edb9ff718741108eb",
+    "url": "./static/js/2.2a9caca0.chunk.js"
+  },
+  {
+    "revision": "f0aa783b05b4b937f16a66c1d4fcc629",
+    "url": "./static/js/2.2a9caca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06cafa38bbd6d2c2e07",
+    "url": "./static/js/main.35f0731c.chunk.js"
+  },
+  {
+    "revision": "4a91f2f82bfab550fb74",
+    "url": "./static/js/runtime-main.964f444c.js"
+  },
+  {
+    "revision": "1efc88a68e50a3868cec4051248f04fa",
+    "url": "./static/media/accord.1efc88a6.jpg"
+  },
+  {
+    "revision": "9567468e2fab04a64420961fc82e86e4",
+    "url": "./static/media/altis.9567468e.jpg"
+  },
+  {
+    "revision": "16dfea294584e064841d1d1439a591d6",
+    "url": "./static/media/camry.16dfea29.jpg"
+  },
+  {
+    "revision": "a03cef2bc5a28bdc19274eacf1245a37",
+    "url": "./static/media/city.a03cef2b.jpg"
+  },
+  {
+    "revision": "63f5ef8c91799347a92fca10ee6d1d50",
+    "url": "./static/media/civic.63f5ef8c.jpg"
+  },
+  {
+    "revision": "8e72cbd4ee7e0b9fdffaecb144341f4f",
+    "url": "./static/media/corolla.8e72cbd4.jpg"
+  },
+  {
+    "revision": "d35c89b4a287fbf6eb317b4f3cccdc53",
+    "url": "./static/media/yaris.d35c89b4.jpg"
+  }
+]);
