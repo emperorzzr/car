463b7f38383c2304fa9241532545aa2a7109a572
emperorzzr <firm48576@gmail.com>
2024-08-25 15:52:39 +0700

    Add files via upload

---
+++ precache-manifest.76143569c87c07947ffca07fe7e36d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f6c605bf935f510a5420f2bf29c39f",
+    "url": "./Dashboard/index.html"
+  },
+  {
+    "revision": "d7eaafbd3d84907d3c44",
+    "url": "./Dashboard/static/css/main.c93c0307.chunk.css"
+  },
+  {
+    "revision": "d3f8db73b39bceeb7809",
+    "url": "./Dashboard/static/js/2.a5986be9.chunk.js"
+  },
+  {
+    "revision": "f0aa783b05b4b937f16a66c1d4fcc629",
+    "url": "./Dashboard/static/js/2.a5986be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7eaafbd3d84907d3c44",
+    "url": "./Dashboard/static/js/main.1ae1107b.chunk.js"
+  },
+  {
+    "revision": "92327c538905ee8c6c98",
+    "url": "./Dashboard/static/js/runtime-main.71882b82.js"
+  }
+]);
